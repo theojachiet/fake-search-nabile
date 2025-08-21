256e77350f0e3b44a2f63f8d61e152f442a29b38
Teoslip <tjachiet@gmail.com>
2025-08-21 10:52:58 +0200

    add search button func and details

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const searchInput = document.querySelector("#search-input");
+const searchButton = document.querySelector('.google-search-btn');
 
 searchInput.addEventListener("keydown", function(event){
  if(event.code === "Enter") {
         search();
     }	
 });
+
+searchButton.addEventListener('click', search);
 
 function search() {
     window.open('./results.html');
